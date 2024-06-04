f7fcbb081d0b5b896c14dae35282fef1b8587e50
Mark Chindudzi <chindudzim@africau.edu>
2024-06-04 17:58:47 +0200

    this python script opens and closes your camera

---
+++ camera_script.py
@@ -0,0 +1,23 @@
+#Libraries
+import cv2
+import sys
+
+# If statement
+s = 0
+if len(sys.argv) > 1:
+    s = sys.argv[1]
+
+source = cv2.VideoCapture(s)
+
+win_name = 'Camera Preview'
+cv2.namedWindow(win_name, cv2.WINDOW_NORMAL)
+
+# while loop
+while cv2.waitKey(1) != 27: # Escape
+    has_frame, frame = source.read()
+    if not has_frame:
+        break
+    cv2.imshow(win_name, frame)
+
+source.release()
+cv2.destroyWindow(win_name)
